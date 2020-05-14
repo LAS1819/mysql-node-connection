558f25cf22a7baa869016f311c852368bda7cf33
Lluís Aracil Sabater <aracil17652@iesmarenostrum.com>
2020-05-14 22:58:47 +0200

    Añadida configuración en server.js

--- src/config/server.js
+++ src/config/server.js
@@ -4,10 +4,24 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Configuraciones
+// -----------Configuraciones----------
+
+// Abrimos el puerto y lo dejamos escuchando en el puerto, si no, en el 3000
 app.set(app.set('port'), process.env.PORT || 3000);
 
+// Definimos el motor de plantilla que vamos a utilizar
+app.set('view engine', 'ejs');
+
+// Definimos dónde están las vistas (usamos módulo path)
+app.set('views', path.join(__dirname, '../app/views'));
 
 
 
-// Middleware
+
+// ------------Middleware----------------------
+
+// Definimos bodyParser, para pasar datos sin imágenes
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Exportamos esto al index.js
+module.exports = app;
